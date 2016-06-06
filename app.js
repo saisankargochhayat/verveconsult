99a30851ac670decb9dd6b81c5d79e5a2e706595
Sandeep <sandeep.mahapatra.5@gmail.com>
2016-06-06 15:42:48 +0530

    added mailer

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 var compression = require('compression');
 
 app.use(compression()); //use compression
-
+var sendgrid  = require('sendgrid')('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');
+var sendmail = function(){};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +26,70 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//-------------------------------Contact Us function----------------------------
+
+app.post('/contactus',function(req,res,next){
+  console.log(req.body);
+  //Email construction for brookhaven
+  var a = parseInt(req.body.checkHuman_a);
+  var b = parseInt(req.body.checkHuman_b);
+  var c = parseInt(req.body.senderHuman);
+  console.log(a + ' + '+b+ ' == '+c);
+  if(a+b !== c){
+    console.log("Captcha check failed");
+    res.satus = 500;
+    res.send("Not human ?");
+  }else{
+    var email     = new sendgrid.Email({
+      to:       'brook16haven@gmail.com',
+      toname : 'BrookHaven',
+      from:     req.body.email,
+      fromname: req.body.name,
+      subject:  'Contact Us from BrookHaven',
+      replyto : req.body.email,
+      text:     'Message from '+ req.body.name+' < '+req.body.email+ ' > '
+      + ' : ' + req.body.message,
+      html: '<h3> Message from ' + req.body.name + ' < '+req.body.email+ ' > '
+      + ' : </h3> <br><h2>'+req.body.message+ '</h2>'
+    });
+    //sending email to brookhaven
+    sendgrid.send(email, function(err, json) {
+      if (err) { console.log(err);
+        res.status = 500;
+        res.send('There was some problem. Please try again later.');
+      }else{
+        console.log(json);
+        //if succesfully sent the mail , send an email to the user
+        //mail construction for user
+        var email     = new sendgrid.Email({
+          to:       req.body.email,
+          toname : req.body.name,
+          from:     'brook16haven@gmail.com',
+          fromname: 'BrookHaven',
+          subject:  'Contact Us from BrookHaven',
+          replyto : 'brook16haven@gmail.com',
+          text:     'Thank You for contacting us. We will get back to you shortly.',
+          html: '<h1> Thank You for contacting us. We will get back to you shortly.</h1> '
+        });
+
+
+
+        //send mail to user
+        sendgrid.send(email, function(err, json) {
+          if (err) { console.log(err);
+            res.status = 500;
+            res.send('There was some problem. Please try again later.');
+          }else{
+            //if succesfull , send success message
+            console.log(json);
+            res.send({'status' : 'success'});
+          }
+        });
+      }
+    });
+  }
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
